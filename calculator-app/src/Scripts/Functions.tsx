ff34d437e84b0e7145c3d6cbe12f7f60adb44b2c
Marc Scattolin <marcscattolin@gmail.com>
2024-10-26 20:36:48 -0400

    implementation of arccos and test

--- calculator-app/src/Scripts/Functions.tsx
+++ calculator-app/src/Scripts/Functions.tsx
@@ -68,3 +68,25 @@ function calcExp(b: number, x: number): number {
         return b * calcExp(b, x - 1);
     }
 }
+
+let A_terms: number[] = [1.5707963050, -0.2145988016, 0.0889789874, -0.0501743046,
+    0.0308918810, -0.0170881256, 0.0066700901, -0.0012624911];
+export function arcCos(x: number): number {
+    let negative: boolean = x < 0.0;
+    x = Math.abs(x);
+    let result: number = 0.0;
+    let x_power_n: number = 1.0;
+    for (let a of A_terms) {
+        result += a * x_power_n;
+        x_power_n *= x;
+    }
+    result = Math.sqrt(1 - x) * result;
+
+    if (negative) {
+        // If negative input, need to use the result
+        // we got from the positive value and convert
+        return Math.PI - result;
+    } else {
+        return result;
+    }
+}
