6488071c05cf9fa8b01c3e8a2a791208caa54a61
Marc Scattolin <marcscattolin@gmail.com>
2024-11-08 18:33:27 -0500

    other fixes for merge

--- calculator-app/src/Components/ContentScreen.tsx
+++ calculator-app/src/Components/ContentScreen.tsx
@@ -1,14 +1,10 @@
-import React from "react";
+import React , {ReactNode} from "react";
 import "./ContentScreen.css";
 
-const ContentScreen: React.FC<{ hasContent: boolean }> = ({ hasContent }) => {
+const ContentScreen: React.FC<{ content: ReactNode }> = ({ content }) => {
     return (
       <div className="content-screen">
-        {hasContent ? (
-          <p>Your future content here</p> // This would be your dynamic content area
-        ) : (
-          <p>No content available</p>
-        )}
+        {content}
       </div>
     );
   };
